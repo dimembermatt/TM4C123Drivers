31f778ba9d6c4c3c9bfaf4570cbc2bd5d43a2927
dariojimenezp <dario_jimenezp@utexas.edu>
2021-07-16 20:18:08 -0500

    Added/modified methods

--- lib/DistanceSensor/DistanceSensor.c
+++ lib/DistanceSensor/DistanceSensor.c
@@ -2,12 +2,45 @@
 #include <stdint.h>
 #include <lib/DistanceSensor/DistanceSensor.h>
 #include <lib/Timers/Timers.h>
+#include <lib/GPIO/GPIO.h>
 
-void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t pin){
-    sensor->pin = pin;
+//TODO: ADD a "initialized" field to DistanceSensor_t to check if not initialize (Maybe)
+
+void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t OUT_pin, GPIOConfig_t EN_pin){
+    sensor->OUT = OUT_pin;
+    sensor->EN = EN_pin;
+
+    GPIOInit(sensor->EN);
+    GPIOSetBit(sensor->EN.GPIOPin, 1);
+    sensor->isEnabled = 1;
+
+}
+
+void DistanceSensor_Enable(DistanceSensor_t* sensor){
+
+    if(sensor->isEnabled != 1){
+    GPIOSetBit(sensor->EN.GPIOPin, 1);
+    sensor->isEnabled = 1;
+    }
+
+}
+
+void DistanceSensor_Disable(DistanceSensor_t* sensor){
+
+    if(sensor->isEnabled != 0){
+    GPIOSetBit(sensor->EN.GPIOPin, 0);
+    sensor->isEnabled = 0;
+    }
+
+}
+
+SensorStatus_t DistanceSensor_ShowStatus(DistanceSensor_t sensor){
+    if(sensor.isEnabled = 1) return ENABLED;
+
+    else return DISABLED;
 }
 
 void DistanceSensor_Read(DistanceSensor_t* sensor){
-    ReadAnalogPort(sensor->pin, &(sensor->data));
+    ReadAnalogPort(sensor->OUT, &(sensor->data));
 }
 
--- lib/DistanceSensor/DistanceSensor.h
+++ lib/DistanceSensor/DistanceSensor.h
@@ -3,14 +3,31 @@
 
 #include <stdint.h>
 #include <lib/ADC/ADC.h>
+#include <lib/GPIO/GPIO.h>
+
 
 typedef struct DistanceSensor{
-    AnalogPort_t pin;
+    AnalogPort_t OUT;
+    GPIOConfig_t EN; //enable pin
+    int8_t isEnabled;
     uint16_t data;
 }DistanceSensor_t;
 
+typedef enum{
+    ENABLED,
+    DISABLED
+}SensorStatus_t;
 
-void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t pin);
+
+//TODO: Maybe add a function that shows if sensor is enabled or disabled
+
+void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t pin, GPIOConfig_t EN);
+
+void DistanceSensor_Enable(DistanceSensor_t sensor);
+
+void DistanceSensor_Disable(DistanceSensor_t sensor);
+
+SensorStatus_t DistanceSensor_ShowStatus(DistanceSensor_t sensor);
 
 void DistanceSensor_Read(DistanceSensor_t* sensor);
 
