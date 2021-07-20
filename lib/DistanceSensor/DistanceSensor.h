fc8c1d71c1f8f004c29502b75d18dff0dd770290
dariojimenezp <dario_jimenezp@utexas.edu>
2021-07-19 23:32:14 -0500

    implemented interrupt method

--- lib/DistanceSensor/DistanceSensor.h
+++ lib/DistanceSensor/DistanceSensor.h
@@ -4,13 +4,17 @@
 #include <stdint.h>
 #include <lib/ADC/ADC.h>
 #include <lib/GPIO/GPIO.h>
+#include <lib/Timers/Timers.h>
 
 
 typedef struct DistanceSensor{
     AnalogPort_t OUT;
     GPIOConfig_t EN; //enable pin
-    int8_t isEnabled;
-    uint16_t data;
+    uint8_t isEnabled;
+    uint8_t isInterrupt;
+    uint8_t interruptNum;
+    TimerConfig_t* interruptConfig;
+    uint16_t* data;
 }DistanceSensor_t;
 
 typedef enum{
@@ -23,15 +27,17 @@ typedef enum{
 
 void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t pin, GPIOConfig_t EN);
 
-void DistanceSensor_Enable(DistanceSensor_t sensor);
+void DistanceSensor_Enable(DistanceSensor_t* sensor);
 
-void DistanceSensor_Disable(DistanceSensor_t sensor);
+void DistanceSensor_Disable(DistanceSensor_t* sensor);
 
 SensorStatus_t DistanceSensor_ShowStatus(DistanceSensor_t sensor);
 
 void DistanceSensor_Read(DistanceSensor_t* sensor);
 
-void DistanceSensor_SetInterrupt(uint16_t period, uint16_t* data);
+void DistanceSensor_SetInterrupt(DistanceSensor_t* sensor, uint16_t freq, uint8_t priority, TimerID_t timer);
 
+void distanceSensor_DisableInterrupt()
 #endif
+
 
