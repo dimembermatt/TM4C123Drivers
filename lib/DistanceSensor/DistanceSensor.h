d7f525ca785aafcaf64bd3421e35a024e388fa42
dariojimenezp <dario_jimenezp@utexas.edu>
2021-07-20 19:26:14 -0500

    reverted to previous commit, fixed errors
    Had too many compile errors in previous commit, so I reverted to an older commit to start over again.

--- lib/DistanceSensor/DistanceSensor.h
+++ lib/DistanceSensor/DistanceSensor.h
@@ -3,19 +3,14 @@
 
 #include <stdint.h>
 #include <lib/ADC/ADC.h>
-<<<<<<< HEAD
 #include <lib/GPIO/GPIO.h>
-#include <lib/Timers/Timers.h>
 
 
 typedef struct DistanceSensor{
     AnalogPort_t OUT;
     GPIOConfig_t EN; //enable pin
-    uint8_t isEnabled;
-    uint8_t isInterrupt;
-    uint8_t interruptNum;
-    TimerConfig_t* interruptConfig;
-    uint16_t* data;
+    int8_t isEnabled;
+    uint16_t data;
 }DistanceSensor_t;
 
 typedef enum{
@@ -26,27 +21,16 @@ typedef enum{
 
 //TODO: Maybe add a function that shows if sensor is enabled or disabled
 
-void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t pin, GPIOConfig_t EN);
+void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t OUT_pin, GPIOConfig_t EN_pin);
 
 void DistanceSensor_Enable(DistanceSensor_t* sensor);
 
 void DistanceSensor_Disable(DistanceSensor_t* sensor);
-=======
 
-typedef struct DistanceSensor{
-    AnalogPort_t pin;
-    uint16_t data;
-}DistanceSensor_t;
-
->>>>>>> parent of 31f778b (Added/modified methods)
-
-void DistanceSensor_Init(DistanceSensor_t* sensor, AnalogPort_t pin);
+SensorStatus_t DistanceSensor_ShowStatus(DistanceSensor_t sensor);
 
 void DistanceSensor_Read(DistanceSensor_t* sensor);
 
-void DistanceSensor_SetInterrupt(DistanceSensor_t* sensor, uint16_t freq, uint8_t priority, TimerID_t timer);
+void DistanceSensor_SetInterrupt(uint16_t period, uint16_t* data);
 
-void distanceSensor_DisableInterrupt()
 #endif
-
-
