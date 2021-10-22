312f4d6294167e0f3c1deca7ea821a89423d7821
dariojimenezp <dario_jimenezp@utexas.edu>
2021-10-21 20:17:41 -0500

    Finished implementing ColorSensor driver and example

--- rasexamples/ColorSensor/keil/ColorSensorExample.c
+++ rasexamples/ColorSensor/keil/ColorSensorExample.c
@@ -1,9 +1,9 @@
 /**
  * File name: ColorSensorExample.c
  * Devices: TM4C123
- * Description: Red on-board LED is turned on when color sensor detects a red color
+ * Description: Red, blue, or green board LEDs turn on if sensdor detects a red, blue, or green color object respectively. 
  * Authors: Dario Jimenez
- * Last Modified: 9/18/21
+ * Last Modified: 10/18/21
  */
 
 /** General imports. These can be declared the like the example below. */
@@ -28,32 +28,81 @@ void EnableInterrupts(void);    // Defined in startup.s
 void DisableInterrupts(void);   // Defined in startup.s
 void WaitForInterrupt(void);    // Defined in startup.s
 
-
 /** Your main execution loop. */
 int main(void) {
-    /** Hardware and software initializations. */
-
+	
     /* Clock setup. */
     PLLInit(BUS_80_MHZ);
     DisableInterrupts();
+		
+		/* configuration for red built-in */
+		GPIOConfig_t redLED = {
+			.pin=PIN_F1,
+			.isOutput=true,
+			.pull=GPIO_PULL_DOWN,
+		};
+		
+		/* configuration for blue built-in */
+		GPIOConfig_t blueLED = {
+			.pin=PIN_F2,
+			.isOutput=true,
+			.pull=GPIO_PULL_DOWN,
+		};
+		
+		/* configuration for green built-in */
+		GPIOConfig_t greenLED = {
+			.pin=PIN_F3,
+			.isOutput=true,
+			.pull=GPIO_PULL_DOWN,
+		};
+		
+		/* Initialize GPIO pins */
+		GPIOInit(redLED);
+		GPIOInit(blueLED);
+		GPIOInit(greenLED);
 
+		/* I2C configuration */
     I2CConfig_t i2ccon =  {
 				.module=I2C_MODULE_0, // This uses pins PB2 (SCL) and PB3 (SDA).
-				.speed=I2C_SPEED_400_KBPS};
+				.speed=I2C_SPEED_400_KBPS //baud rate of 400 kilobits per second
+		};
 		
-		TCS34725Config_t config = {
-		.I2CConfig= i2ccon, 
-		.Gain=TCS34725_Gain_1X, 
-		.IntegrationTime=TCS34725_INT_TIME_2MS,
-		.isLong=false};
+		/* color sensor configuration */
+		ColorSensorConfig_t config = {
+			.I2CConfig= i2ccon, 
+			.isInterrupt=true, //use interrupt 
+			.samplingFrequency = 200, //200 hz
+			.timerID=TIMER_1A //timer 1A
+		};
 		
-		TCS34725_t sensor = TCS34725Init(config);
+		/* Initialize color sensor */
+		ColorSensor_t sensor = ColorSensorInit(config);
+		
+		EnableInterrupts();
     
     /** Main loop. Put your program here! */
     while (1) {
-
-      TCS34725Read(&sensor);
-			//DelayMillisec(500);
-			 
+			
+			/* if all RGB values are under 0x10 (no object is likely in front of the sensor) turn off all LEDs */
+			if( (sensor.RedValue < 0x10) && (sensor.BlueValue < 0x10) && (sensor.GreenValue < 0x10) ){
+				GPIOSetBit(PIN_F1, 0);
+				GPIOSetBit(PIN_F2, 0);
+				GPIOSetBit(PIN_F3, 0);
+			}
+			
+			else{
+				/* turns on red LED if highest RGB value is red (i.e. a red object is placed in front of sensor) */
+				if((sensor.RedValue> sensor.GreenValue) && (sensor.RedValue > sensor.BlueValue)) GPIOSetBit(PIN_F1, 1);
+				else GPIOSetBit(PIN_F1, 0);
+				
+				/* turns on blue LED if highest RGB value is blue (i.e. a blue object is placed in front of sensor) */
+				if((sensor.BlueValue> sensor.RedValue) && (sensor.BlueValue > sensor.GreenValue)) GPIOSetBit(PIN_F2, 1);
+				else GPIOSetBit(PIN_F2, 0);
+				
+				/* turns on green LED if highest RGB value is green (i.e. a green object is placed in front of sensor) */
+				if((sensor.GreenValue> sensor.RedValue) && (sensor.GreenValue > sensor.BlueValue)) GPIOSetBit(PIN_F3, 1);
+				else GPIOSetBit(PIN_F3, 0);
+									 
+			}
     }
 }
--- raslib/ColorSensor/ColorSensor.c
+++ raslib/ColorSensor/ColorSensor.c
@@ -28,23 +28,22 @@
 #include <lib/Timer/Timer.h>
 #include <rasinc/I2C3.h>
 
-
-#define MAX_COLORSENSOR_TIMERS 4 // Max number of interrupts (1 for each color)
-#define MAX_COLORSENSOR_FREQ 30 //max frequency for color sensor
-#define COLORSENSOR_PRIORITY 4
-
+#define COLORSENSOR_PRIORITY 6 //interrupt priority for color sensors
+#define MAX_COLOR_SENSORS 4 //there are only 4 I2C modules, hence there can only be a maximum of 4 color sensors at once
 #define TCS34725_ADDRESS 0x29 //I2C Slave Adress for TCS34725
 
+/* Communication Data */
 #define TCS34725_COMMAND 0x80 //Command bit for command register
 #define TCS34725_ENABLE_PON 0x01 //Power On bit on Enable Register: writing 1 activates internal oscillator, 0 disables it
 #define TCS34725_ENABLE_AEN 0x02 //RGBC Enable bit on Enable Register. 1: enable TCS34725 ADC. 0: disable TCS34725 ADC
 #define TCS34725_ENABLE_WEN 0x08 //Wait Enable bit on Enable Register. 1: activates wait timer. 0: disables it.
 #define TCS34725_ENABLE_AIEN 0x10 //RGBC Interrupt Enable bit on Enable Register. 1: permits interrupts. 0: does not permit interrupt
-#define TCS34725_ATIME_10 0xF6 // integration of 10 cycles
+#define TCS34725_ATIME_1 0xFF // integration of 1 cycles
 #define TCS34725_CONTROL_1X_GAIN 0x00 //1x gain for ADC
 #define TCS34725_STATUS_AVALID 0x01 //indicates whether ADC has finished integration cycle
 #define TCS34725_PERS_5CYCLES 0x04 //makes interrupt not trigger until 5 consecutive values past threshold
 
+/* ID numbers for the TCS34725 Color Sensor */
 #define TCS34725_ID1 0x44 //TCS34725 and TCS34721 ID number
 #define TCS34725_ID2 0x4D //TCS34723 and TCS34727 ID number
 
@@ -71,77 +70,111 @@
 #define TCS34725_BDATAL_R 0x1A //Blue data low byte
 #define TCS34725_BDATAH_R 0x1B //Blue data high byte
 
-static GPIOConfig_t INT_Pin ={PIN_A7, GPIO_TRI_STATE}; //GPIO configuration for int pin
-static TimerConfig_t timersConfig[MAX_COLORSENSOR_TIMERS]; //array of timer of configuration for each color sensor
-static Timer_t timers[MAX_COLORSENSOR_TIMERS]; //array of timer for each color sensor
-//static ColorSensor_t* sensors[MAX_COLORSENSOR_TIMERS] = {0x0, 0x0, 0x0, 0x0}; //array of sensors used for interrupts
+/* index for color sensor settings */
+static int index = 0; 
 
-/* interrupt counters for persistance buffer */
-static uint8_t redCount = 0;
-static uint8_t greenCount = 0;
-static uint8_t blueCount = 0;
+/** color sensor settings for ColorSensorHandler **/
+static struct ColorSensorSettings{
+	
+	/** pointer to color sensor **/
+	ColorSensor_t * sensor;
 
-//indicates if I2C is on
-uint8_t isI2COn = 0;
+	/** timer used for color sensor **/
+	Timer_t timer;
 
-/* values to transmit for setting up color sensor */
-static uint8_t EnableWriteValues[2] = {TCS34725_COMMAND | TCS34725_ENABLE_R, TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN};
-static uint8_t ATimeWriteValues[2] = {TCS34725_COMMAND | TCS34725_ATIME_R, TCS34725_ATIME_10};
-static uint8_t ControlWriteValues[2] = {TCS34725_COMMAND | TCS34725_CONTROL_R, TCS34725_CONTROL_1X_GAIN};
+	bool isEnabled;
+}settings[MAX_COLOR_SENSORS];
 
-/* Values to transmit for setting up clear interrupt */
-static uint8_t setInterruptValues[2] = {TCS34725_COMMAND | TCS34725_ENABLE_R, TCS34725_ENABLE_AIEN | TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN};
-static uint8_t persistenceValues[2] = { TCS34725_COMMAND | TCS34725_PERS_R, TCS34725_PERS_5CYCLES};
+/**
+ * @brief interrupt handler for color sensor interrupts 
+ **/
+void ColorSensorHandler(uint32_t * args){
+	struct ColorSensorSettings * setting = ((struct ColorSensorSettings *)args);
 
+	ColorSensorRead(setting->sensor);
+	
+}
 
+/**
+ * @brief Initializes the TCS34725 color sensor
+ * 
+ * @param Configuration for color sensor
+ * 
+ * @return An ColorSensor_t instance that can be used for sampling/reading RGB values
+ **/
+ColorSensor_t ColorSensorInit(ColorSensorConfig_t config){
+	I2C_t i2c = I2CInit(config.I2CConfig);
 
-TCS34725_t TCS34725Init(TCS34725Config_t config){
-  I2C_t i2c = I2CInit(config.I2CConfig);
-
-  /* check device ID */
-  uint8_t TransmitData[2] = {TCS34725_COMMAND | TCS34725_ID_R, config.Gain};
-  uint8_t read = 0;
+	/* check device ID */
+	uint8_t TransmitData[2] = {TCS34725_COMMAND | TCS34725_ID_R, TCS34725_CONTROL_1X_GAIN};
+	uint8_t read = 0;
 
 	I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 1);
 	I2CMasterReceive(i2c, TCS34725_ADDRESS, &read, 1);
 
+
+	/* if ID doesn't match, then incorrect sensor was plugged in */
+	if(read != TCS34725_ID1 && read != TCS34725_ID2){ 
+		assert(0);
+	}
+
+	/* transmit gain settings */
+	TransmitData[0] = TCS34725_COMMAND | TCS34725_CONTROL_R;
+	I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
+
+	/* transmit integration time settings */
+	TransmitData[0] =  TCS34725_COMMAND | TCS34725_ATIME_R;
+	TransmitData[1] = TCS34725_ATIME_1;
+	I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
+
+
+	/* transmit Enable settings (enables the sensor) */
+	TransmitData[0] = TCS34725_COMMAND | TCS34725_ENABLE_R;
+	TransmitData[1] = TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN;
+	I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
+
+	/*configure interrupt if enabled */
+	if(config.isInterrupt == true){
+
+		
+		/* configuration for interrupt */
+		TimerConfig_t intConfig = {
+			.timerID=config.timerID,
+			.period=freqToPeriod(config.samplingFrequency, MAX_FREQ),
+			.timerTask=ColorSensorHandler,
+			.isPeriodic=true,
+			.priority=COLORSENSOR_PRIORITY,
+			.timerArgs=(uint32_t *)&settings[index]
+		};
+		
+		/* store sensor setting for handler use */
+		settings[index].timer = TimerInit(intConfig);
+		settings[index].isEnabled = true;
+	}
 	
-  if(read != TCS34725_ID1 && read != TCS34725_ID2){ //if ID is the same, then incorrect sensor is plugged in
-    assert(0);
-  }
+	/* create instance of sensor */
+	ColorSensor_t sensor = {i2c,0,0,0,0};
 
-  /* transmit gain settings */
-  TransmitData[0] = TCS34725_COMMAND | TCS34725_CONTROL_R;
-  I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
+	/* store instance of sensor in sensor settings dor handler use */
+	if(config.isInterrupt == true){
+		settings[index].sensor = &sensor;
+		TimerStart(settings[index].timer);
+		index++;
+	}
 
-  /* transmit integration time settings */
-  TransmitData[0] =  TCS34725_COMMAND | TCS34725_ATIME_R;
-  TransmitData[1] = config.IntegrationTime;
-  I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
-
-  /* Transmit long integration time setting*/
-  if(config.isLong == true){
-      TransmitData[0] =  TCS34725_COMMAND | TCS34725_CONFIG_R;
-      TransmitData[1] = 1;
-      I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
-  }
-
-  /* transmit Enable settings (enables the sensor) */
-  TransmitData[0] = TCS34725_COMMAND | TCS34725_ENABLE_R;
-  TransmitData[1] = TCS34725_ENABLE_PON | TCS34725_ENABLE_AEN | TCS34725_ENABLE_WEN;
-  I2CMasterTransmit(i2c, TCS34725_ADDRESS, TransmitData, 2);
-
-  TCS34725_t sensor = {i2c,0,0,0,0,0,0,0,0};
-
-  return sensor;
+	return sensor;
 }
 
-void TCS34725Read(TCS34725_t * sensor){
+/**
+ * @brief Samples/updates the RGBC values from color sensor 
+ * 
+ * @param ColorSensor_t instance passed as a pointer
+ **/
+void ColorSensorRead(ColorSensor_t * sensor){
 
-	
-  volatile uint8_t dataL[] = { 0 };
-  volatile uint8_t dataH[] = { 0 };
-  volatile uint16_t data;
+  uint8_t dataL[] = { 0 };
+  uint8_t dataH[] = { 0 };
+  uint16_t data;
 
   /* Read clear bytes */
   uint8_t transmit[] = { TCS34725_COMMAND| TCS34725_CDATAH_R};
@@ -160,7 +193,7 @@ void TCS34725Read(TCS34725_t * sensor){
 	
 	/* Read red bytes */
 	
-  transmit[0] = TCS34725_COMMAND| TCS34725_RDATAH_R;
+  	transmit[0] = TCS34725_COMMAND| TCS34725_RDATAH_R;
 	I2CMasterTransmit(sensor->i2c, TCS34725_ADDRESS, transmit, 1);
 	I2CMasterReceive(sensor->i2c, TCS34725_ADDRESS, dataH, 1);
 	transmit[0] = TCS34725_COMMAND| TCS34725_RDATAL_R;
@@ -176,7 +209,7 @@ void TCS34725Read(TCS34725_t * sensor){
 	
 	/* Read green bytes */
 	
-  transmit[0] = TCS34725_COMMAND| TCS34725_GDATAH_R;
+  	transmit[0] = TCS34725_COMMAND| TCS34725_GDATAH_R;
 	I2CMasterTransmit(sensor->i2c, TCS34725_ADDRESS, transmit, 1);
 	I2CMasterReceive(sensor->i2c, TCS34725_ADDRESS, dataH, 1);
 	transmit[0] = TCS34725_COMMAND| TCS34725_GDATAL_R;
@@ -205,6 +238,30 @@ void TCS34725Read(TCS34725_t * sensor){
 	sensor->BlueValue = data;
 	
 	data = 0;
+}
+
+/**
+ * @brief Disabled interrupt for Color Sensor
+ **/
+void ColorSensorDisableInterrupt(ColorSensor_t sensor){
+
+	for(int i=0; i<MAX_COLOR_SENSORS; i++){
+		if(&sensor == settings[i].sensor){
+			if(settings[i].isEnabled) TimerStop(settings[i].timer);
+			return;
+		}
+	}
+}
+
+/**
+ * @brief Enables interrupt for Color Sensor if an interrupt was initialized
+ **/
+void ColorSensorEnableInterrupt(ColorSensor_t sensor){
 	
-	
+	for(int i=0; i<MAX_COLOR_SENSORS; i++){
+		if(&sensor == settings[i].sensor){
+			if(!settings[i].isEnabled) TimerStart(settings[i].timer);
+			return;
+		}
+	}
 }
